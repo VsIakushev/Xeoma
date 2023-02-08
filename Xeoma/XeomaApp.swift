422e95ee4c7ba09277b271c9da8ee6c2f9edcad5
Vitalii Iakushev <vsiakushev@gmail.com>
2023-02-08 18:53:33 +0400

    Initial Commit

---
+++ Xeoma/XeomaApp.swift
@@ -0,0 +1,17 @@
+//
+//  XeomaApp.swift
+//  Xeoma
+//
+//  Created by Vitaliy Iakushev on 08.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct XeomaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
